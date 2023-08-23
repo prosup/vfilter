d8f7fa0f50d98e4b39acebc5dcabec714fa30bb1
prosup <prosupcn@msn.com>
2023-08-23 02:43:49 +0000

    add class SERVER

--- solv.py
+++ solv.py
@@ -1,11 +1,63 @@
 #!/usr/bin/env python3
 import sqlite3
-import csv
+#import csv
 import os.path
 import os
+import base64
 
+#validate server from database
+#we need multiple thread access server port,to determine the server is alive
+class SERVER:
 
-class SERVER_DB():
+    def __init__(self,raw):
+        self.raw=raw
+        
+
+    def rawData(self):
+        return self.raw
+    
+    def solvConfig(self):
+        self.config=base64.b64decode(self.raw)
+        
+#write tmp.ovpn for grep & awk
+        dpath="/tmp/vfilter/"
+        mode = 0o777
+        try :
+            os.mkdir(dpath,mode)
+        except Exception as e :
+            print(e)
+        fpath=dpath+"tmp.ovpn"
+        try:
+            cfile=open(fpath,"wb+")
+            cfile.write(self.config)
+        except Exception as e:
+            print(e)
+        self.solvePort()
+        self.solveAddr()
+        self.solveProtocol()
+        os.rename(fpath,dpath+self.addr+".ovpn")
+    
+    def solveProtocol(self):
+        cmd="cat /tmp/vfilter/tmp.ovpn | grep ^proto | awk '{print $2}'"
+        self.protocol=os.popen(cmd).readline().split("\n")[0]
+        return 
+
+    def solvePort(self):
+        cmd="cat /tmp/vfilter/tmp.ovpn | grep ^remote | awk '{print $3}'"
+        self.port=os.popen(cmd).readline().split("\n")[0]
+        return 
+
+    def solveAddr(self):
+        cmd="cat /tmp/vfilter/tmp.ovpn | grep ^remote | awk '{print $2}'"
+        self.addr=os.popen(cmd).readline().split("\n")[0]
+        return 
+
+    def validate(self):
+
+        return
+
+## manage server database
+class SERVER_DB:
     con = sqlite3.connect("vfilter.db")
     cur = con.cursor()
 
@@ -32,6 +84,11 @@ class SERVER_DB():
         where IP in (select IP from server group by IP having count(IP) > 1) 
         and rowid not in (select min(rowid) from server group by IP having count(IP) > 1 )'''
 
+    def validateServer(self,row):
+        sv=SERVER(row)
+        sv.solvConfig()
+
+        return
 
     def validateDatabase(self):#if the datebase is avalible
         if not os.path.isfile("vfilter.db"):
@@ -66,7 +123,7 @@ class SERVER_DB():
             except Exception as e:
                 print(e)
         return
-
+    #delete server by it's index
     def delServer(self,id):#delete one line of server,if the server is deprecated
         self.cur.execute(self.delete_rowid+id) 
         self.con.commit()
@@ -79,6 +136,7 @@ class SERVER_DB():
     def addServer(self,s):
         row=s.split(",")
         if self.validateRow(row):
+            self.validateServer(row[14])
             self.cur.execute(self.insert_records,row)
         return 
 
@@ -89,6 +147,7 @@ class SERVER_DB():
         for line in list:
             try:
 #                self.cur.execute()
+#                self.validateServer(line)
                 self.addServer(line)
             except Exception as e:
                 print(e)
@@ -106,12 +165,12 @@ class SERVER_DB():
     def configCache(self):
 
         return
+    def itor(self):
 
+        return
 
 def main():
-#    createdb()
-#    getserverlist("178.249.213.39:24288")
-#    getserverlist("222.255.11.117:54621")
+
     sdb=SERVER_DB()
     sdb.updateDatabase("222.255.11.117:54621")
 #    sdb.delServer("1")
